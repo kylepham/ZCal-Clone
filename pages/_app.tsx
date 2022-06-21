186afa87052952f6182eed1a03fe9a3fb4f8a024
Hoang Pham <hoanghuypham2121@gmail.com>
2022-06-21 15:47:07 -0400

    Generate hooks for queries and mutations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,23 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Nav from "../components/Nav";
+import { useMemo } from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const client = useMemo(() => {
+    return new ApolloClient({
+      cache: new InMemoryCache(),
+      uri: `${process.env.VERCEL_URL || "http://localhost:3000"}/api/graphql`,
+    });
+  }, []);
+
   return (
     <SessionProvider session={pageProps.session}>
-      <Nav />
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Nav />
+        <Component {...pageProps} />
+      </ApolloProvider>
     </SessionProvider>
   );
 }
